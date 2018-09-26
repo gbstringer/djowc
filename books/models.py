ecece17e1adf9cf6dd2154bfd28173d103bb3bb7
Gary Stringer <g.b.stringer@exeter.ac.uk>
2018-09-26 12:55:49 +0100

    Further experiments with access control - error message not currently being called in either guise
    Signed-off-by: Gary Stringer <g.b.stringer@exeter.ac.uk>

--- books/models.py
+++ books/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from unittest.util import _MAX_LENGTH
 
 # Create your models here.
 
@@ -7,6 +6,7 @@ ROLECHOICE = (
     ('author','Author'),
     ('contributor','Contributor'),
     ('editor','Editor'),
+    ('translator', 'Translator'),
     )
 
 class Book(models.Model):
